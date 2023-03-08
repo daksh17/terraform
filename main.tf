e793f15a042213c3971ad00e50b018dfeb0ea5d9
Daksh <dakshgaur56@gmail.com>
2023-03-08 07:10:13 -0800

    provisioners

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 provider "aws" {
 
   region = "eu-central-1"
-   access_key = "AKIASIBNDTD4FVBWQZHL"
-  secret_key = "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"
+   #access_key = "AKIASIBNDTD4FVBWQZHL"
+  #secret_key = "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"
 }
 
 
@@ -14,6 +14,7 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_location" {}
+variable "private_key_location" {}
 
 
 resource "aws_vpc" "daksh_vpc" {
@@ -240,9 +241,34 @@ public_key = file(var.public_key_location)
    availability_zone = var.avail_zone
    associate_public_ip_address = true
     key_name = aws_key_pair.ssh_key.key_name
-   user_data =  file("entry-script.sh")
+   
+   
+   #user_data =  file("entry-script.sh")
 
- 
+    connection { 
+
+      type="ssh"
+      host=self.public_ip
+      user="ec2-user"
+      private_key=file(var.private_key_location)
+
+    }
+
+    provisioner "file"{
+      source = "entry-script.sh"
+      destination = "/home/ec2-user/entry-script-on-ec2.sh"
+    }
+
+  provisioner "remote-exec" {
+         script  = file("/home/ec2-user/entry-script-on-ec2.sh")
+   
+  }
+
+  provisioner "local-exec"{
+    command = "echo ${self.public_ip}> output.txt"
+  }
+
+
  tags = {
 
     Name : "${var.env_prefix}-default-inst"
